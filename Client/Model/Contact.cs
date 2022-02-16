de658629c85166f2987673874bf8bc9ee383205c
Piotr Uhl <piotruhl@gmail.com>
2022-02-16 01:18:43 +0100

    Kontakty z nieodczytanymi wiadomościami są wyróżnione

--- Client/Model/Contact.cs
+++ Client/Model/Contact.cs
@@ -1,16 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Client.Model {
-	public class Contact {
+	public class Contact : INotifyPropertyChanged {
+
+		#region INotifyPropertyChanged
+		public event PropertyChangedEventHandler PropertyChanged;
+		private void NotifyPropertyChanged(string name) {
+			if (PropertyChanged != null)
+				PropertyChanged(this, new PropertyChangedEventArgs(name));
+		}
+		#endregion
+
 		[Key]
 		public int Id { get; set; }
-		public string DisplayName { get; set; }
-		public bool New { get; set; } = false;
+		private string displayName;
+		public string DisplayName {
+			get => displayName;
+			set {
+				displayName = value;
+				NotifyPropertyChanged("DisplayName");
+			}
+		}
+		private bool neww;
+		public bool New {
+			get => neww;
+			set {
+				neww = value;
+				NotifyPropertyChanged("New");
+			}
+		}
 
 		public virtual List<Message> Messages { get; set; }
 	}
