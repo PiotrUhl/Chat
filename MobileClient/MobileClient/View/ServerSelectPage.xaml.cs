66027972adb3be1e89e26c1553b41677fe36e701
Piotr Uhl <piotruhl@gmail.com>
2022-08-21 22:46:12 +0200

    Added mobile client project

---
+++ MobileClient/MobileClient/View/ServerSelectPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MobileClient.View {
+	public partial class ServerSelectPage : ContentPage {
+		public ServerSelectPage() {
+			InitializeComponent();
+		}
+
+		private async void Button_Clicked(object sender, EventArgs e) {
+			await Navigation.PushAsync(new LoginPage());
+		}
+	}
+}
