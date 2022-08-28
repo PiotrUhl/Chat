409a03476caad6e220e7b588cbb9b48e436acb62
Piotr Uhl <piotruhl@gmail.com>
2022-08-28 18:41:39 +0200

    Implemented data bindings on contact list page

---
+++ MobileClient/MobileClient/Model/Contact.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MobileClient.Model {
+	public class Contact {
+		public long Id { get; set; }
+		public string Name { get; set; }
+	}
+}
---
+++ MobileClient/MobileClient/ViewModel/List.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Text;
+using Xamarin.Forms;
+
+namespace MobileClient.ViewModel {
+	public class List : INotifyPropertyChanged {
+
+		#region INotifyPropertyChanged
+		public event PropertyChangedEventHandler PropertyChanged;
+		private void NotifyPropertyChanged(string name) {
+			if (PropertyChanged != null)
+				PropertyChanged(this, new PropertyChangedEventArgs(name));
+		}
+		#endregion
+
+		public ObservableCollection<Model.Contact> ContactList { get; set; }
+		private Model.Server selectedContact = null;
+		public Model.Server SelectedContact {
+			get => selectedContact;
+			set {
+				if (selectedContact != value) {
+					selectedContact = value;
+					NotifyPropertyChanged("SelectedContact");
+				}
+			}
+		}
+
+		public Command SettingsCommand { get; private set; }
+		public Command<int> ContactCommand { get; private set; }
+
+		public List() {
+			SettingsCommand = makeSettingsCommand();
+			ContactCommand = makeContactCommand();
+
+			ContactList = new ObservableCollection<Model.Contact>();
+			ContactList.Add(new Model.Contact {
+				Id = 1,
+				Name = "Kontakt 1"
+			});
+			ContactList.Add(new Model.Contact {
+				Id = 2,
+				Name = "Kontakt 2"
+			});
+			ContactList.Add(new Model.Contact {
+				Id = 3,
+				Name = "Kontakt 3"
+			});
+			ContactList.Add(new Model.Contact {
+				Id = 4,
+				Name = "Kontakt 4"
+			});
+			ContactList.Add(new Model.Contact {
+				Id = 5,
+				Name = "Kontakt 5"
+			});
+		}
+
+		private Command makeSettingsCommand() {
+			return new Command(
+				execute: async () => {
+					await Application.Current.MainPage.Navigation.PushAsync(new View.SettingsPage());
+				},
+				canExecute: () => {
+					return true;
+				}
+			);
+		}
+
+		private Command<int> makeContactCommand() {
+			return new Command<int>(
+				execute: async (int id) => {
+					await Application.Current.MainPage.Navigation.PushAsync(new View.ConversationPage());
+				},
+				canExecute: (int id) => {
+					return true;
+				}
+			);
+		}
+	}
+}
